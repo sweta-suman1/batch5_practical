daaf55261a8b11b76c810c2b0255cef29b17bf29
sweta <work.college.swetaW@gmail.com>
2024-03-12 09:25:01 +0530

    commiting cpp

---
+++ add.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int add(int a,int b)
+{
+	return(a+b);
+}
+
+int main()
+{
+	int x,y,result;
+	cout<<"Enter the value for x :";
+	cin>>x;
+	cout<<"\n Enter the value for y :";
+	cin>>y;
+	result=add(x,y);
+	cout<<"\n addition of x & y is :"<<result;
+	
+	return 0;
+}
